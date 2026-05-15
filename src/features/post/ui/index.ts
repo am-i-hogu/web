8dfee144a22fb93adf35a7da60ef13a33cd7d3ef
reizvoll <mara2277@naver.com>
2026-05-15 13:50:09 +0900

    fix: 불필요한 export 파일 제거

--- src/features/post/ui/index.ts
+++ src/features/post/ui/index.ts
@@ -1,6 +1,3 @@
 export * from "../constants/post-filter.constants";
 export * from "./contents-delete-modal";
-export * from "./post-comments-section";
-export * from "./post-detail-card";
 export * from "./post-filter-bottom-sheet";
-export * from "./post-vote-section";
