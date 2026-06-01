968d0656f25051e5d1adb8077719a9923f6982c1
mj0107 <alswns1285@gmail.com>
2026-06-01 10:07:46 +0900

    style: 20자 이상에서 2자 이상으로 오타 변경 #33

--- src/app/onboarding/_models/schema.ts
+++ src/app/onboarding/_models/schema.ts
@@ -10,7 +10,7 @@ export const onboardingSchema = z.object({
       message: "닉네임을 입력해주세요.",
     })
     .min(2, "2자 이상 20자 이하의 한글, 영문, 숫자만 사용해주세요.")
-    .max(20, "20자 이상 20자 이하의 한글, 영문, 숫자만 사용해주세요.")
+    .max(20, "2자 이상 20자 이하의 한글, 영문, 숫자만 사용해주세요.")
     .regex(/^[a-zA-Z0-9가-힣ㄱ-ㅎㅏ-ㅣ]+$/, "특수문자는 입력할 수 없습니다.")
     .regex(/^[a-zA-Z0-9가-힣]+$/, "단어 또는 문장 형태로 입력해주세요.")
     .refine((val) => !ALREADY_USED_NICKNAMES.has(val), {
